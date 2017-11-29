d9d109b552fcb224a2895cb1fd7bc88b5d0d9236
bhupinderkumarbl <bhupinder.kumar@blinfosoft.com>
2017-11-29 14:53:50 +0530

    delete student added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
 
+export class NewUserInfo {
+  name: string;
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -11,5 +15,34 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
   }
+  // public userData: any;
+  // model: NewUserInfo = new NewUserInfo();
 
+  title = 'All Users';
+  users: NewUserInfo[] = [
+    {
+      name: 'Bhupinder',
+      address: 'Mohali',
+      email: 'Bhupinder@gmail.com',
+    },
+    {
+      name: 'Mona',
+      address: 'Chandigarh',
+      email: 'mona@gmail.com',
+    },
+    {
+      name: 'Shifali',
+      address: 'Mohali',
+      email: 'Shifali@gmail.com',
+    }
+  ];
+  /****add new user to list****/
+  addNewUser(userData : NewUserInfo){
+    this.users.push(userData);
+  }
+  /****delete user from list****/
+  deleteNewUser(user: any){
+    const index: number = this.users.indexOf(user);
+    this.users.splice(index, 1);
+  }
 }
